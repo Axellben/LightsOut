dd75b1cff8cb219a12dae77e31cc98de34ca3901
Axellben <cretu.alexandru2000@gmail.com>
2020-06-26 15:09:34 +0300

    Bug fixed

---
+++ build/precache-manifest.e27a46b8175bce3cc6bba40b3d186012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3e9a8775b1e27d9ce8e",
+    "url": "/Axellben/LightsOut/static/js/runtime~main.b3e9a877.js"
+  },
+  {
+    "revision": "6476ecbcd3c4c9229cfc",
+    "url": "/Axellben/LightsOut/static/js/main.6476ecbc.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/Axellben/LightsOut/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "6476ecbcd3c4c9229cfc",
+    "url": "/Axellben/LightsOut/static/css/main.62934e5c.chunk.css"
+  },
+  {
+    "revision": "08de70df180f1b2f737ba617a400ad5b",
+    "url": "/Axellben/LightsOut/index.html"
+  }
+];
